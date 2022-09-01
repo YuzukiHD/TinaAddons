38166d456d0ec688a9339153d4d75090db2160e8
柚木鉉 <740291272@qq.com>
2022-09-01 21:51:40 +0800

    Update apply_patch.sh

--- apply_patch.sh
+++ apply_patch.sh
@@ -6,8 +6,11 @@ echo -e "\n**********************************\nPrepare Environment... **!!!ROOT 
 sudo apt-get install build-essential subversion git libncurses5-dev zlib1g-dev gawk flex quilt libssl-dev xsltproc libxml-parser-perl mercurial bzr ecj cvs unzip lib32z1 lib32z1-dev lib32stdc++6 libstdc++6 libc6:i386 libstdc++6:i386 lib32ncurses5 lib32z1 -y
 
 echo -e "\n**********************************\nApply T113 Patch...\n***********************************\n"
-rm -rf .gitignore
-git apply 0001-add-t113-platform-to-d1-h-sdk.patch
+
+FILE=&SOURCE_PATH/rules.mk
+if [ -f "$FILE" ]; then
+  git apply 0001-add-t113-platform-to-d1-h-sdk.patch
+fi
 
 echo -e "\n**********************************\nFix Patch Error...\n**********************************\n"
 rm -rf $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
@@ -17,6 +20,7 @@ echo -e "\n**********************************\nAdd Toolchains...\n**************
 wget https://github.com/YuzukiHD/TinaAddons/releases/download/1.0/arm-toolchains.tar.gz
 tar xf arm-toolchains.tar.gz
 
-echo -e "\n**********************************\nStart Tina Linux\n**********************************\n"
-source build/envsetup.sh 
-lunch
+echo -e "\n**********************************\nDone, Start Tina Linux\n**********************************\n"
+rm -rf 0001-add-t113-platform-to-d1-h-sdk.patch
+rm -rf arm-toolchains.tar.gz
+rm -rf apply_patch.sh
