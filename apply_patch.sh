46c44fad086f69dddc7dc34f1bf09cb954669af4
柚木鉉 <740291272@qq.com>
2022-09-01 21:59:40 +0800

    Update apply_patch.sh

--- apply_patch.sh
+++ apply_patch.sh
@@ -7,20 +7,22 @@ sudo apt-get install build-essential subversion git libncurses5-dev zlib1g-dev g
 
 echo -e "\n**********************************\nApply T113 Patch...\n***********************************\n"
 
-FILE=&SOURCE_PATH/rules.mk
+FILE=$SOURCE_PATH/rules.mk
 if [ -f "$FILE" ]; then
   git apply 0001-add-t113-platform-to-d1-h-sdk.patch
+  echo -e "\n**********************************\nFix Patch Error...\n**********************************\n"
+  
+  rm -rf $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
+  echo -e "// Copyright 2010 Google Inc. All Rights Reserved." > $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
+
+  echo -e "\n**********************************\nAdd Toolchains...\n**********************************\n"
+  wget https://github.com/YuzukiHD/TinaAddons/releases/download/1.0/arm-toolchains.tar.gz
+  tar xf arm-toolchains.tar.gz
+
+  echo -e "\n**********************************\nDone, Start Tina Linux\n**********************************\n"
+  rm -rf 0001-add-t113-platform-to-d1-h-sdk.patch
+  rm -rf arm-toolchains.tar.gz
+  rm -rf apply_patch.sh
+else
+  echo -e "ERROR: NOT THE ROOT OF TINA LINUX!"
 fi
-
-echo -e "\n**********************************\nFix Patch Error...\n**********************************\n"
-rm -rf $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
-echo -e "// Copyright 2010 Google Inc. All Rights Reserved." > $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
-
-echo -e "\n**********************************\nAdd Toolchains...\n**********************************\n"
-wget https://github.com/YuzukiHD/TinaAddons/releases/download/1.0/arm-toolchains.tar.gz
-tar xf arm-toolchains.tar.gz
-
-echo -e "\n**********************************\nDone, Start Tina Linux\n**********************************\n"
-rm -rf 0001-add-t113-platform-to-d1-h-sdk.patch
-rm -rf arm-toolchains.tar.gz
-rm -rf apply_patch.sh
